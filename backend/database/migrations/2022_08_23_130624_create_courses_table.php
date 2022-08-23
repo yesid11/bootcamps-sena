ff19ba1e60801a6267c77f7b9f01c2d48df4ae1e
Your Name <you@example.com>
2022-08-23 09:18:51 -0500

    evidencia/23/08/2022

---
+++ backend/database/migrations/2022_08_23_130624_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->integer('weeks');
+            $table->float('enroll_cost', 5,5);
+            $table->string('minimun_skill');
+            $table->foreignId('bootcamp_id')->constrained();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
